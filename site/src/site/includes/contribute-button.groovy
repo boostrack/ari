0a0166efb2e344ceb34234c3ffa629f5208ebca2
Madvillain <madvillain@gigfork.com>
2014-08-24 20:28:20 +0200

    iteration 10 - angular

--- site/src/site/includes/contribute-button.groovy
+++
@@ -1,7 +0,0 @@
-div(id: 'contribute-btn') {
-    button(type: 'button', class: 'btn btn-default',
-            onclick: "window.location.href=\"https://github.com/boostrack/ari/tree/gh-pages/${currentPage}.html\"") {
-        i(class: 'fa fa-pencil-square-o') {}
-        yield ' Improve this page'
-    }
-}
